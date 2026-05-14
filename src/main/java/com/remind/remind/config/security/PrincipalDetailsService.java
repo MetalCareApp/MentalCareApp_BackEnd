c45e25256a83b2ed96884561f8d36e81aa282cf5
gkseorlsa <gkseorlsa@gmail.com>
2026-05-14 14:29:20 +0900

    refactor: 스프링 Security 인증 사용자 모델 개선

--- src/main/java/com/remind/remind/config/security/PrincipalDetailsService.java
+++ src/main/java/com/remind/remind/config/security/PrincipalDetailsService.java
@@ -15,9 +15,9 @@ public class PrincipalDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다: " + username));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다: " + email));
         
         return new PrincipalDetails(user);
     }
